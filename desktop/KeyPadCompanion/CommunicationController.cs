f86535036dd5afbd895212286523bc8d9d853040
Dmitry Byankin <the.immago@gmail.com>
2024-06-01 23:50:08 +0300

    Desktop app: implement basic commands

--- desktop/KeyPadCompanion/CommunicationController.cs
+++ desktop/KeyPadCompanion/CommunicationController.cs
@@ -7,9 +7,24 @@ namespace KeyPadCompanion
 {
     class CommunicationController
     {
+        public enum ButtonClickType
+        {
+            Single,
+            Double,
+            Long
+        }
+
         // Version
         public delegate void VersionResponseHandler(string version);
         public event VersionResponseHandler? OnVersionResponse;
+
+        // Led state
+        public delegate void LedResponseHandler(int index, int mode, byte r, byte g, byte b, int speed);
+        public event LedResponseHandler? OnLedResponse;
+
+        // Button
+        public delegate void ButtonClickHandler(ButtonClickType type, int index);
+        public event ButtonClickHandler? OnButtonClick;
 
         private SerialPort? port;
 
@@ -28,17 +43,24 @@ namespace KeyPadCompanion
             if (port == null) { return; }
 
             // ToDo: Try Catch
-            port.Open();
+            try
+            {
+                port.Open();
 
-            // Wait for connection
-            Thread.Sleep(200);
+                // Wait for connection
+                Thread.Sleep(200);
 
-            // Read some USBtoSerial junk
-            port.ReadExisting();
-            Thread.Sleep(200);
+                // Read some USBtoSerial junk
+                port.ReadExisting();
+                Thread.Sleep(200);
 
-            // Only after this subscribe to data
-            port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+                // Only after this subscribe to data
+                port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
         }
 
         public void Stop()
@@ -60,6 +82,19 @@ namespace KeyPadCompanion
             port?.WriteLine("v");
         }
 
+        public void GetLed(int index)
+        {
+            if (port == null) { return; }
+            if (!port.IsOpen) { return; }
+            port?.WriteLine($"getled {index}");
+        }
+
+        public void SetLed(int index, int mode, byte r, byte g, byte b, int speed)
+        {
+            if (port == null) { return; }
+            if (!port.IsOpen) { return; }
+            port?.WriteLine($"setled {index} {mode} {r} {g} {b} {speed}");
+        }
 
         // - Private
         private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
@@ -69,19 +104,53 @@ namespace KeyPadCompanion
             string text = port.ReadLine();
             string[] parts = text.Split(' ');
 
-            Debug.Write(text);
+            //Debug.WriteLine(text);
 
             if (parts.Length == 0) { return; }
             string command = parts[0];
 
             // Version
-            if (command == "ver")
+            if (command == "ver" && parts.Length >= 2)
             {
                 string version = parts[1];
                 OnVersionResponse?.Invoke(version);
             }
 
-            
+            // Led
+            if (command == "led" && parts.Length >= 7)
+            {
+                // led (N) (mode) (r) (g) (b) (speed)
+                // led 1 1 255 255 255 255
+
+                int index = Int32.Parse(parts[1]);
+                int mode = Int32.Parse(parts[2]);
+                byte r = Byte.Parse(parts[3]);
+                byte g = Byte.Parse(parts[4]);
+                byte b = Byte.Parse(parts[5]);
+                int speed = Int32.Parse(parts[6]);
+                OnLedResponse?.Invoke(index, mode, r, g, b, speed);
+            }
+
+            // Single click
+            if (command == "sclick" && parts.Length >= 2)
+            {
+                int index = Int32.Parse(parts[1]);
+                OnButtonClick?.Invoke(ButtonClickType.Single, index);
+            }
+
+            // Double click
+            if (command == "dclick" && parts.Length >= 2)
+            {
+                int index = Int32.Parse(parts[1]);
+                OnButtonClick?.Invoke(ButtonClickType.Double, index);
+            }
+
+            // Long click
+            if (command == "lclick" && parts.Length >= 2)
+            {
+                int index = Int32.Parse(parts[1]);
+                OnButtonClick?.Invoke(ButtonClickType.Long, index);
+            }
         }
     }
 }
--- desktop/KeyPadCompanion/MainWindow.xaml.cs
+++ desktop/KeyPadCompanion/MainWindow.xaml.cs
@@ -46,8 +46,49 @@ namespace KeyPadCompanion
             communicationController?.Stop();
             communicationController = new CommunicationController(port);
             communicationController.OnVersionResponse += CommunicationController_OnVersionResponse;
+            communicationController.OnLedResponse += CommunicationController_OnLedResponse;
+            communicationController.OnButtonClick += CommunicationController_OnButtonClick;
             communicationController.Start();
             communicationController.GetVersion();
+            communicationController.GetLed(0);
+
+            // TODO;
+            communicationController.SetLed(0, 0, 255, 50, 0, 1000);
+            communicationController.SetLed(1, 0, 50, 255, 0, 1000);
+            communicationController.SetLed(2, 0, 0, 50, 255, 1000);
+        }
+
+        private void CommunicationController_OnButtonClick(CommunicationController.ButtonClickType type, int index)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                string text = $"[{DateTime.Now.ToString("dd/MM/yy HH:mm")}] ";
+                switch (type)
+                {
+                    case CommunicationController.ButtonClickType.Single:
+                        text += $"Button #{index} single click";
+                        break;
+                    case CommunicationController.ButtonClickType.Double:
+                        text += $"Button #{index} double click";
+                        break;
+                    case CommunicationController.ButtonClickType.Long:
+                        text += $"Button #{index} long click";
+                        break;
+                }
+                lastReceivedCommandLabel.Content = text;
+            });
+        }
+
+        private void CommunicationController_OnLedResponse(int index, int mode, byte r, byte g, byte b, int speed)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                // Button 1
+                if (index == 0)
+                {
+                    button1ColorRqctangle.Fill = new SolidColorBrush(Color.FromRgb(r, g, b));
+                }
+            });
         }
 
         private void CommunicationController_OnVersionResponse(string version)
@@ -56,6 +97,11 @@ namespace KeyPadCompanion
             {
                 versionLabel.Content = $"ver: {version}";
             });
+        }
+
+        private void button1ColorRqctangle_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Debug.WriteLine("TODO");
         }
     }
 }
